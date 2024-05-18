d0a9592690eed5dbd3ff974889284e211184e2d2
Edison asare <Edisonpryce@gmail.com>
2024-05-17 18:27:56 -0700

    updated

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+
+""" Initializes  the module global variables """
+
+from .engine.file_storage import FileStorage
+""" Retrieving the storage instance """
+storage = FileStorage()
+storage.reload()
