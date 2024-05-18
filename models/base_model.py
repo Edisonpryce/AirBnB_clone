95212e1e8014d2af0e1d729ce1eb1fa01ac45b0e
Edison asare <Edisonpryce@gmail.com>
2024-05-17 17:59:38 -0700

    update

--- models/base_model.py
+++ models/base_model.py
@@ -2,12 +2,56 @@
 
 from uuid import uuid4
 from datetime import datetime
+import models
 
 class BaseModeel:
-    def __init__(self):
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.update_at = datetime.now()
+    """Base class to be inheritted by all classes"""
+
+    def __init__(self, *args, **kwargs):
+        """ serialize and deserialize class """
+
+        """initialize this if nothing passed"""
+        if kwargs == {}:
+            self.id = str(uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
+            models.storage.new(self)
+            return
+
+        """deserialize of kwargs"""
+        if 'id' not in kwargs:
+            kwargs['id'] = str(uuid4())
+        self.id = kwargs['id']
+
+        for Key, val in kwargs.items():
+            if Key == "__class_":
+                continue
+        if "created_at" in kwargs:
+            self.created_at = datetime.strptime(
+                    kwargs['created_at'],
+                    '%Y-%m-%dT%H:%M:%S.%f')
+        if "updated_at" in kwargs:
+            self.updated_at = datetime.strptime(
+                    kwargs['updated_at'],
+                    '%Y-%m-%dT%H:%M:%S.%f')
 
     def __str__(self):
-        pass
+        """overide str representation of self"""
+        fmt = "[{}] ({}) {}"
+        return fmt.format(
+                type(self).__name__,
+                self.id,
+                self.__dict__)
+
+    def save(self):
+        """updating last updated variable"""
+        self.updated_at = datetime.utcnow()
+        models.storage.save()
+
+    def to_dict(self):
+        """Returns a dictionary representation of self"""
+        temp = {**self.__dict__}
+        temp['__class__'] = type(self).__name__
+        temp['created_at'] = self.created_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
+        temp['updated_at'] = self.updated_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
+        return temp
